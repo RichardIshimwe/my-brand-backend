1c48fcd51aca2df60dbbf2fbe46d22ff7cc0a3c2
rich26638 <ishimwerichard26@gmail.com>
2023-02-28 10:03:27 +0200

    athentication and authorization

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -2,14 +2,28 @@ import signup from '../models/signup.models.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import response from '../utils/response.util.js'
+import joi from 'joi'
 
 class login {
 
   static async checkUser(req, res) {
-    const { email, password } = req.body;
-    const check = await signup.findOne({ email });
+   const { email, password } = req.body;
+   const validateLogin = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required()
+   })
+
     try {
-      if (email !== "" && password !== "" && check !== null) {
+
+     const {error,value} = validateLogin.validate({email,password},{ abortEarly: false })
+       if(error){
+        const errorMessage = error.details.map((detail) =>{
+          return detail.message;
+        });
+        return res.status(400).json({message:errorMessage})
+       }
+       const check = await signup.findOne({ email });
+      if (check !== null) {
         const checkPassword = await bcrypt.compare(password, check.password);
         if (check !== null && checkPassword === false) {
           return response.error(res, 400, "email or password is incorrect")
@@ -19,11 +33,11 @@ class login {
         const decoded = jwt.verify(asignToken, process.env.SECRET_KEY);
         const { username, admin } = decoded;
         return response.success(res, 200, `${username} you are welcome........`);
-      } else if (check == null) {
+      } 
+      else 
+      {
         return response.error(res, 400, "you have to do a signup");
-      } else {
-        return response.error(res, 400, "*all fields are required");
-      }
+      } 
     } catch (error) {
       console.log(error)
       return response.error(res, 500, "internal server error");
