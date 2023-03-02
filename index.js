48a815224e2cc14be2325e5dcdfdcf55a6d66aa0
rich26638 <ishimwerichard26@gmail.com>
2023-03-02 07:41:34 +0200

    index file updated

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import allRoutes from './routes/all.routes.js'
+// import allRoutes from ''
+import allRoutes from './src/routes/all.routes.js'
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import response from './utils/response.util.js';
+import response from './src/utils/response.util.js';
 
 const app = express();
 dotenv.config();
@@ -17,14 +18,14 @@ app.use(allRoutes);
 const port = process.env.PORT;
 mongoose.set('strictQuery', true);
 
-const con = () => mongoose.connect(`${process.env.MONGODBURL}`, { useNewUrlParser: true, useUnifiedTopology: true });
-const listen = () => app.listen(port)
+ mongoose.connect(`${process.env.MONGODBURL}`, { useNewUrlParser: true, useUnifiedTopology: true });
+ app.listen(port);
 
 // Promise.all([con()])
-Promise.all([con(),listen()])
-    .then(() => {
-    //  console.log(`mongodb connected and app lisening at http://localhost:${port}`)
-    })
-    .catch((error) => {return response.error(res, 500, "internal server erro")})
+// Promise.all([con(),listen()])
+//     .then(() => {
+//     //  console.log(`mongodb connected and app lisening at http://localhost:${port}`)
+//     })
+//     .catch((error) => {return response.error(res, 500, "internal server erro")})
 
-export default app
+// export default app
