be26be80621f4d6d77ced5cd8cd4f173981bfa83
rich26638 <ishimwerichard26@gmail.com>
2023-03-04 20:35:19 +0200

    package.json changed

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 // import allRoutes from ''
-import allRoutes from './src/routes/all.routes.js'
+import allRoutes from './routes/all.routes.js'
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import response from './src/utils/response.util.js';
+import response from './utils/response.util.js';
 
 const app = express();
 dotenv.config();
