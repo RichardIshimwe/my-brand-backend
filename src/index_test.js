01e0b1b6f5a37fd09f6cafe9cac925fb407c34fd
rich26638 <ishimwerichard26@gmail.com>
2023-03-06 16:02:37 +0200

    first commit

---
+++ src/index_test.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import allRoutes from './routes/all.routes.js'
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import response from './utils/response.util.js';
+
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.get('/',(req, res) => response.success(res, 200,"welcome to the back-end of my project"));
+app.use(allRoutes);
+const port = process.env.PORT;
+mongoose.set('strictQuery', true);
+mongoose.connect(`${process.env.TEST_MONGODBURL}`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+export default app
--- test/logout.test.js
+++ test/logout.test.js
@@ -1,21 +1,12 @@
-import index from '../index.js'
+import index from '../src/index_test.js'
 import request from 'supertest'
 
 describe('Test all routes',() =>{
-  // let mongoServer;
-  let server;
-  beforeAll(async () =>{
-    server = index.listen(2000);
-  })
-  afterAll((end) =>{
-    server.close(end)
-  })
  test('test logout Route', async () =>{
   const response = await request(index).post('/logout');
   expect(response.status).toBe(200);
   expect(response.body).toEqual({message:"You are logged out"})
  })
-
 })
 
 
