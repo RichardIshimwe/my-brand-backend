02ca5907982da6aeebcb0c292a8d474aa5ee85a9
rich26638 <ishimwerichard26@gmail.com>
2023-03-09 09:33:00 +0200

    github action workflow

--- test/response.test.js
+++
@@ -1,7 +0,0 @@
-import response from "../src/utils/response.util.js";
-
-describe('should respond to the user request', () =>{
-    it('Should return success message', () =>{
-        
-    })
-})
---
+++ test/simple.test.js
@@ -0,0 +1,10 @@
+import request from 'supertest';
+import app from '../index_test.js';
+import bcrypt from 'bcrypt';
+
+
+  describe('signupUser', () => {
+    it('should signup a user if he/she does not have account', async () => {
+          expect(1+1).toBe(2);
+})});
+// node --experimental-vm-modules jest/bin/jest.js --verbose --runInBand --forceExit
