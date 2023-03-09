69d13884e6ac727c7c59ba7f065011d8e34c518a
rich26638 <ishimwerichard26@gmail.com>
2023-03-09 10:22:30 +0200

    providing token

--- src/utils/response.util.js
+++ src/utils/response.util.js
@@ -3,7 +3,7 @@ class response {
   static success(res, status, message, data) {
     res.status(status).json({
       message: message,
-      data: data
+      data: data,
     })
   }
   static error(res, status, message) {
