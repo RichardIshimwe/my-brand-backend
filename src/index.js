52d1a921ecb0f5f24c2ecc39cdab1d63ca62a51f
rich26638 <ishimwerichard26@gmail.com>
2023-03-12 10:37:13 +0200

    configuring cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import docs from './documentation/index.js'
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'header');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
