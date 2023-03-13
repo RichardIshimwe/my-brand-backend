a64ff21670d069e8bb7256a6c5e6e26ba85170ce
rich26638 <ishimwerichard26@gmail.com>
2023-03-13 12:06:28 +0200

    delete blog functionality

--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -5,8 +5,9 @@ import isAdmin from '../middlewares/isAdmin.middleware.js'
 const router = express.Router();
 
 router.get('/user', blogControler.userBlogs);
-router.delete('/:id', isAdmin, blogControler.deleteBlog)
-router.put('/:id', blogControler.editBlog)
+router.delete('/:id', blogControler.deleteBlog);
+// router.delete('/:id', isAdmin, blogControler.deleteBlog)
+router.put('/:id', blogControler.editBlog);
 router.post('/', blogControler.createBlog);
 // router.post('/', verifyUser, blogControler.createBlog);
 
